ed5c296e5a2f43b99f7b4cd5f55601b26b2ede3c
flaviocafe <75816376+flaviocafe@users.noreply.github.com>
2025-07-16 14:19:06 +0000

    adiciona scripts e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>O amor é lindo demais e muito mesmo</h1>
+  return <h1>O amor é lindo demais e muito mesmo</h1>;
+}
+
+function teste2() {
+  console.log("prettier");
 }
 
 export default Home;
